7154abf385d8bd3f2eb4dcac25b7ddb03920c494
yousan <mx364730@alibaba-inc.com>
2022-04-05 18:34:09 +0800

    fix(webpack): 修复webpack 提交问题

---
+++ rename.sh
@@ -0,0 +1,15 @@
+git filter-branch --env-filter '
+OLD_EMAIL="mx364730@alibaba-inc.com"
+CORRECT_NAME="Maxuan"
+CORRECT_EMAIL="786609202@qq.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+export GIT_COMMITTER_NAME="$CORRECT_NAME"
+export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+export GIT_AUTHOR_NAME="$CORRECT_NAME"
+export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
